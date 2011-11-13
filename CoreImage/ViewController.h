babe28a25feb47cda3a5cc82935b93d8e5ce6727
iimgal <iimgal@163.com>
2011-11-13 16:40:39 +0800

    Initial Commit

---
+++ CoreImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreImage
+//
+//  Created by  on 11-11-13.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
